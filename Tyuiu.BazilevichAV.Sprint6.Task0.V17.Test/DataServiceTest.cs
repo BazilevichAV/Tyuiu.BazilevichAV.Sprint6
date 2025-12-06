3e71d2e20dbaae45acdbcf02bbab1a90e26393f5
BazilevichAV <Tyuiu.BazilevichAV@outlook.com>
2025-12-06 15:25:29 +0500

    Добавьте файлы проекта.

---
+++ Tyuiu.BazilevichAV.Sprint6.Task0.V17.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.BazilevichAV.Sprint6.Task0.V17.Lib;
+namespace Tyuiu.BazilevichAV.Sprint6.Task0.V17.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCalc()
+        {
+            DataService ds = new DataService();
+            int x = 3;
+            double res = ds.Calculate(x);
+            double wait = 6.695;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
