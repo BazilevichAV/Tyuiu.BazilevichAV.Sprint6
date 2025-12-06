abc6810475c1f895efe2a1c5eb57da465fa982e0
BazilevichAV <Tyuiu.BazilevichAV@outlook.com>
2025-12-06 16:27:48 +0500

    Task1

---
+++ Tyuiu.BazilevichAV.Sprint6.Task1.V19.Lib/DataService.cs
@@ -0,0 +1,22 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint6;
+namespace Tyuiu.BazilevichAV.Sprint6.Task1.V19.Lib
+{
+    public class DataService : ISprint6Task1V19
+    {
+        public double[] GetMassFunction(int startValue, int stopValue)
+        {
+            int len = stopValue - startValue + 1;
+            double[] res = new double[len];
+
+            int index = 0;
+            for (int x = startValue; x <= stopValue; x++)
+            {
+                double result = Math.Cos(x) + Math.Cos(x) / x + 2 - 3 * x;
+                res[index] = Math.Round(result, 2);
+                index++;
+            }
+
+            return res;
+        }
+    }
+}
---
+++ Tyuiu.BazilevichAV.Sprint6.Task1.V19.Test/DataServiceTest.cs
@@ -0,0 +1,31 @@
+﻿using Tyuiu.BazilevichAV.Sprint6.Task1.V19.Lib;
+namespace Tyuiu.BazilevichAV.Sprint6.Task1.V19.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCalc()
+        {
+            DataService ds = new DataService();
+            int startValue = -5;
+            int stopValue = 5;
+            double[] res = ds.GetMassFunction(startValue, stopValue);
+            double[] expected =
+            {
+                4.67,   // x=-5
+                0.43,  // x=-4
+                -8.26,  // x=-3
+                -9.87,  // x=-2
+                -3.98,  // x=-1
+                1.00,   // x=0
+                0.02,   // x=1
+                -1.87,  // x=2
+                3.74,  // x=3
+                16.43,  // x=4
+                24.67   // x=5
+            };
+            Assert.AreEqual(11, res.Length);
+        }
+    }
+}
