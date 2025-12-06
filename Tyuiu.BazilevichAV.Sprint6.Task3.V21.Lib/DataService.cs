5e15ff9b3d4b5eda295ef1bf9b6167cfe5a41713
BazilevichAV <Tyuiu.BazilevichAV@outlook.com>
2025-12-06 18:57:51 +0500

    Task3

---
+++ Tyuiu.BazilevichAV.Sprint6.Task3.V21.Lib/DataService.cs
@@ -0,0 +1,32 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint6;
+namespace Tyuiu.BazilevichAV.Sprint6.Task3.V21.Lib
+{
+    public class DataService : ISprint6Task3V21
+    {
+        public int[,] Calculate(int[,] matrix)
+        {
+            int rows = matrix.GetLength(0);
+            int cols = matrix.GetLength(1);
+
+            int[,] resultMatrix = (int[,])matrix.Clone();
+
+            for (int i = 0; i < rows - 1; i++)
+            {
+                for (int j = i + 1; j < rows; j++)
+                {
+                    if (resultMatrix[i, 0] > resultMatrix[j, 0])
+                    {
+                        for (int k = 0; k < cols; k++)
+                        {
+                            int temp = resultMatrix[i, k];
+                            resultMatrix[i, k] = resultMatrix[j, k];
+                            resultMatrix[j, k] = temp;
+                        }
+                    }
+                }
+            }
+
+            return resultMatrix;
+        }
+    }
+}
---
+++ Tyuiu.BazilevichAV.Sprint6.Task3.V21.Test/DataServiceTest.cs
@@ -0,0 +1,42 @@
+﻿using Tyuiu.BazilevichAV.Sprint6.Task3.V21.Lib;
+namespace Tyuiu.BazilevichAV.Sprint6.Task3.V21.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCalc()
+        {
+            DataService ds = new DataService();
+
+            int[,] matrix = new int[5, 5]
+            {
+        {26, -15,   7,   1,  -4},
+        {14,  26,  22,  17,  27},
+        {-8,  31,  22,  34,   4},
+        {-18,  16,  -2,  16,  27},
+        {5,   2,  -4,  16,  15}
+            };
+
+            int[,] result = ds.Calculate(matrix);
+
+            Assert.AreEqual(-18, result[0, 0]);
+            Assert.AreEqual(-8, result[1, 0]);
+            Assert.AreEqual(5, result[2, 0]);
+            Assert.AreEqual(14, result[3, 0]);
+            Assert.AreEqual(26, result[4, 0]);
+
+            Assert.AreEqual(-18, result[0, 0]);
+            Assert.AreEqual(16, result[0, 1]);
+            Assert.AreEqual(-2, result[0, 2]);
+            Assert.AreEqual(16, result[0, 3]);
+            Assert.AreEqual(27, result[0, 4]);
+
+            Assert.AreEqual(26, result[4, 0]);
+            Assert.AreEqual(-15, result[4, 1]);
+            Assert.AreEqual(7, result[4, 2]);
+            Assert.AreEqual(1, result[4, 3]);
+            Assert.AreEqual(-4, result[4, 4]);
+        }
+    }
+}
