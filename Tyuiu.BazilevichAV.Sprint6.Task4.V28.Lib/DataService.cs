2ac9d7cecd8dcc34c505515fdf968b62add0b022
BazilevichAV <Tyuiu.BazilevichAV@outlook.com>
2025-12-07 17:25:16 +0500

    Task4

---
+++ Tyuiu.BazilevichAV.Sprint6.Task4.V28.Lib/DataService.cs
@@ -0,0 +1,32 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint6;
+namespace Tyuiu.BazilevichAV.Sprint6.Task4.V28.Lib
+{
+    public class DataService : ISprint6Task4V28
+    {
+        public double[] GetMassFunction(int startValue, int stopValue)
+        {
+            int len = stopValue - startValue + 1;
+            double[] res = new double[len];
+
+            int index = 0;
+            for (int x = startValue; x <= stopValue; x++)
+            {
+                double result;
+
+                if (x == 0)
+                {
+                    result = 0.0;
+                }
+                else
+                {
+                    result = Math.Cos(2 * x) + Math.Sin(x) / (x + 2.5) + (2 * x);
+                }
+
+                res[index] = Math.Round(result, 2);
+                index++;
+            }
+
+            return res;
+        }
+    }
+}
---
+++ Tyuiu.BazilevichAV.Sprint6.Task4.V28.Test/DataServiceTest.cs
@@ -0,0 +1,35 @@
+﻿using Tyuiu.BazilevichAV.Sprint6.Task4.V28.Lib;
+namespace Tyuiu.BazilevichAV.Sprint6.Task4.V28.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCalc()
+        {
+            DataService ds = new DataService();
+
+            int startValue = -5;
+            int stopValue = 5;
+
+            double[] res = ds.GetMassFunction(startValue, stopValue);
+
+            double[] expectedValues =
+{
+                59.49,   // x = -5
+                38.47,   // x = -4
+                -2.81,   // x = -3
+                -19.67,  // x = -2
+                -8.42,   // x = -1
+                0.0,    // x = 0
+                -6.3,    // x = 1
+                -20.14,  // x = 2
+                -3.51,   // x = 3
+                38.13,   // x = 4
+                59.6     // x = 5
+            };
+
+            Assert.AreEqual(11, res.Length);
+        }
+    }
+}
