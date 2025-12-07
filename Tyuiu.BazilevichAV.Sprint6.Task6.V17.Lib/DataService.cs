6b42a50954554cbfea2b6b0df31e08abd740f3a3
BazilevichAV <Tyuiu.BazilevichAV@outlook.com>
2025-12-07 19:42:44 +0500

    Task6

---
+++ Tyuiu.BazilevichAV.Sprint6.Task6.V17.Lib/DataService.cs
@@ -0,0 +1,31 @@
+﻿using System.Globalization;
+using System.IO;
+using System.Text;
+using tyuiu.cources.programming.interfaces.Sprint6;
+
+namespace Tyuiu.BazilevichAV.Sprint6.Task6.V17.Lib
+{
+    public class DataService : ISprint6Task6V17
+    {
+        public string CollectTextFromFile(string path)
+        {
+
+            var lines = File.ReadAllLines(path);
+            var thirdWords = new List<string>();
+
+            for (int i = 0; i < lines.Length; i++)
+            {
+                var words = lines[i].Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+                if (words.Length >= 3)
+                {
+                    string thirdWord = words[2];
+                    thirdWords.Add(thirdWord);
+                }
+            }
+
+            return string.Join(" ", thirdWords);
+        }
+    }
+}
+
---
+++ Tyuiu.BazilevichAV.Sprint6.Task6.V17.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.ApplicationInsights;
+using Tyuiu.BazilevichAV.Sprint6.Task6.V17.Lib;
+using static System.Net.Mime.MediaTypeNames;
+namespace Tyuiu.BazilevichAV.Sprint6.Task6.V17.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckedExistsFile()
+        {
+            DataService ds = new DataService();
+            string path = @"C:\DataSprint6\InPutDataFileTask6V17.txt";
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExists = fileInfo.Exists;
+            bool wait = true;
+            Assert.AreEqual(wait, fileExists);
+        }
+    }
+}
