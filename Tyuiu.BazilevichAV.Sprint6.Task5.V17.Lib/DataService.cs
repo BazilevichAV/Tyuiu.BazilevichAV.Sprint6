2a1407ce82068a713c86b697680f8a7ca8ccc1ae
BazilevichAV <Tyuiu.BazilevichAV@outlook.com>
2025-12-07 17:53:23 +0500

    Task5

---
+++ Tyuiu.BazilevichAV.Sprint6.Task5.V17.Lib/DataService.cs
@@ -0,0 +1,51 @@
+﻿using System.Globalization;
+using System.IO;
+using System.Text;
+using tyuiu.cources.programming.interfaces.Sprint6;
+
+namespace Tyuiu.BazilevichAV.Sprint6.Task5.V17.Lib
+{
+    public class DataService : ISprint6Task5V17
+    {
+        public double[] LoadFromDataFile(string path)
+        {
+           
+            StringBuilder result = new StringBuilder();
+            List<double> negnum = new List<double>();
+
+            using (StreamReader reader = new StreamReader(path))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    string[] numbers = line.Split(new char[] { ' ', '\t', ',', ';' },
+                                                  StringSplitOptions.RemoveEmptyEntries);
+
+                    foreach (string numberStr in numbers)
+                    {
+                        if (double.TryParse(numberStr.Replace(',', '.'),
+                            NumberStyles.Any,
+                            CultureInfo.InvariantCulture,
+                            out double number))
+                        {
+                            if (number < 0)
+                            {
+                                negnum.Add(number);
+                            }
+                        }
+                    }
+                }
+            }
+
+            // Вывод отрицательных чисел в консоль
+            foreach (double number in negnum)
+            {
+                double roundedNumber = Math.Round(number, 3);
+                Console.WriteLine(roundedNumber.ToString("F3", CultureInfo.InvariantCulture));
+            }
+
+            return negnum.ToArray();
+        }
+    }
+}
+
